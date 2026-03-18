89c455d012270c83a0b806d6363e50e0543b4985
Nathan Flurry <git@nathanflurry.com>
2026-03-18 15:57:28 -0700

    feat: US-162 - Add Vite project-matrix fixture

---
+++ packages/secure-exec/tests/projects/vite-pass/app/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+	return React.createElement("div", null, "Hello from Vite");
+}
+
+createRoot(document.getElementById("root")).render(React.createElement(App));
